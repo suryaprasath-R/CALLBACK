9f3d813134359a4125e7c9db966ea9a21719f156
suriyaprasath <83021622+suryaprasath-R@users.noreply.github.com>
2023-08-31 23:48:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+var counter1 = document.getElementById('counter')
+setTimeout(() => {
+    counter1.innerText = "10";
+    console.log("10");
+     },1000);
+
+
+setTimeout(() => {
+    counter1.innerText = "9";
+    console.log("9");
+}, 2000);   
+     
+
+setTimeout(() => {
+    counter1.innerText = "8";
+    console.log("8");
+}, 3000); 
+     
+setTimeout(() => {
+    counter1.innerText = "7";
+    console.log("7");
+}, 4000);
+     
+setTimeout(() => {
+    counter1.innerText = "6";
+    console.log("6");
+}, 5000); 
+
+setTimeout(() => {
+    counter1.innerText = "5";
+    console.log("5");
+}, 6000);
+
+setTimeout(() => {
+    counter1.innerText = "4";
+    console.log("4");
+}, 7000);
+
+
+setTimeout(() => {
+    counter1.innerText = "3";
+    console.log("3");
+}, 8000);
+
+setTimeout(() => {
+    counter1.innerText = "2";
+    console.log("2");
+}, 9000);
+
+
+setTimeout(() => {
+    counter1.innerText = "1";
+    console.log("1");
+}, 10000);
+
+
+setTimeout(() => {
+    counter1.innerText = "HAPPY INDEPENDENCE DAY";
+    console.log("HAPPY INDEPENDENCE DAY");
+}, 11000);
+
+
+
+
